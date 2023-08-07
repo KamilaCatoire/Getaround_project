b29b3b54c21994a613133f2f22173d19df68e4ce
KamilaCatoire <kamila.catoire@gmail.com>
2023-08-07 17:15:00 +0200

    test api modified

--- FastAPI/python_api_tester.py
+++ FastAPI/python_api_tester.py
@@ -1,7 +1,6 @@
 import requests
 
 response = requests.post("https://fastapi-ga-kc-dd15d3a6caeb.herokuapp.com/predict", json=
-[
   {
     "model_key": "Citroën",
     "mileage": 100000,
@@ -16,22 +15,22 @@ response = requests.post("https://fastapi-ga-kc-dd15d3a6caeb.herokuapp.com/predi
     "has_getaround_connect": True,
     "has_speed_regulator": True,
     "winter_tires": True
-  },
-  {
-    "model_key": "Audi",
-    "mileage": 100000,
-    "engine_power": 150,
-    "fuel": "diesel",
-    "paint_color": "black",
-    "car_type": "convertible",
-    "private_parking_available": True,
-    "has_gps": True,
-    "has_air_conditioning": True,
-    "automatic_car": True,
-    "has_getaround_connect": True,
-    "has_speed_regulator": True,
-    "winter_tires": True
   }
-]
+  # {
+  #   "model_key": "Audi",
+  #   "mileage": 100000,
+  #   "engine_power": 150,
+  #   "fuel": "diesel",
+  #   "paint_color": "black",
+  #   "car_type": "convertible",
+  #   "private_parking_available": True,
+  #   "has_gps": True,
+  #   "has_air_conditioning": True,
+  #   "automatic_car": True,
+  #   "has_getaround_connect": True,
+  #   "has_speed_regulator": True,
+  #   "winter_tires": True
+  # }
+
 )
 print(response.json())
